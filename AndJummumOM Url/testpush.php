b1148d438a4455312454d1f43679d7cd359bcc39
Thidaporn Kijkamjai <jinglejill@me.com>
2018-08-01 11:36:40 +0700

    review url path to use only one place

---
+++ AndJummumOM Url/testpush.php
@@ -0,0 +1,24 @@
+<?php
+    include_once('dbConnect.php');
+    setConnectionValue("AND_JUMMUM_OM");
+    $arrBody = array(
+                     'alert' => 'test'//ข้อความ
+                      ,'sound' => 'default'//,//เสียงแจ้งเตือน
+//                      ,'badge' => 3 //ขึ้นข้อความตัวเลขที่ไม่ได้อ่าน
+                     ,'category' => 'Print'
+                      );
+    sendTestApplePushNotification('9284a675face93a9bcefb2ef8ea51929f3a3771c90e1bda204cf11180e00ef4c',$arrBody);
+    
+//    sleep(5);
+//
+//
+//    $arrBody = array(
+//                     'alert' => 'เทสจิ๋ว 2
+//                     เอ'//ข้อความ
+//                     ,'sound' => 'default'//,//เสียงแจ้งเตือน
+//                     //                      ,'badge' => 3 //ขึ้นข้อความตัวเลขที่ไม่ได้อ่าน
+//                     );
+//    sendTestApplePushNotification('1877301d04f677b7fcc415b7f0bcbd799bf679013b14f76ce746d778087a22f6',$arrBody);
+?>
+
+//<table><tr><td style="text-align: center;border: 1px solid black; padding-left: 10px;padding-right: 10px; border-radius: 15px;">x</td></tr></table>
